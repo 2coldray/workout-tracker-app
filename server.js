58b8f161b9ad2d5e47bbedd11eeb96eab17d13d9
Ray Bray <rbray.bray2@gmail.com>
2020-10-21 14:34:41 -0400

    have basic server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+//Require Packages
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+//Establish Ports
+const PORT = process.env.PORT || 3000;
+
+//Require Controllers
+
+//Express Middleware
+
+//Mongoose Connection
+
+//API Config - check if it works
+
+//Use Controllers
+
+//Listen to Port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
