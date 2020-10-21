002192d5d0375a66c370fc744b782f84271ad05f
Ray Bray <rbray.bray2@gmail.com>
2020-10-21 14:46:42 -0400

    added more components to the server

--- server.js
+++ server.js
@@ -8,13 +8,41 @@ const PORT = process.env.PORT || 3000;
 
 //Require Controllers
 
+
 //Express Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Mongoose Connection
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  );
+  
+  const connection = mongoose.connection;
+  
+  connection.on("connected", () => {
+    console.log("Mongoose successfully connected.");
+  });
+  
+  connection.on("error", (err) => {
+    console.log("Mongoose connection error: ", err);
+  });
 
 //API Config - check if it works
+app.get("/api/config", (req, res) => {
+    res.json({
+      success: true,
+    });
+  });
 
 //Use Controllers
+
 
 //Listen to Port
 app.listen(PORT, () => {
